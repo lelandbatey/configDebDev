a484ee8241116a42f2aa1fe428638eb3e63fe1e3
lelandbatey <lelandbatey@gmail.com>
2012-11-16 12:51:05 -0800

    Extended functionality of install script, future changes made easier.

--- installConfig.sh
+++ installConfig.sh
@@ -7,15 +7,25 @@
 #  <nothing>
 
 
-if [ "$1" == .bashrc ]; then
-  cp .bashrc ~/
+CMDS[1]="cp \"$1\" ~/"
+CMDS[2]="cp -r .vim ~/ & cp .vimrc ~/"
+CMDS[3]=$(cp .bashrc ~/
+cp -r .vim ~/
+cp .vimrc ~/
+cp .Xresources ~/
+cp .dir_colors ~/
+cp .bash_profile ~/)
+
+
+printf "${CMDS[3]}"
+# Because the actual value of CMDS[3] is evaluated as a command (notice the "$()" in it's invocation), I don't know a way to echo that value
+echo "$1"
+
+if [ -n "$1" ] && [ ! "$1" == vim ]; then
+  cp "$1" ~/
 elif [ "$1" == vim ]; then
   cp -r .vim ~/ &
   cp .vimrc ~/
 elif [ -z "$1" ]; then
-  cp .bashrc ~/
-  cp -r .vim ~/
-  cp .vimrc ~/
-  cp .Xresources ~/
-  cp .dir_colors ~/
+  ${CMDS[3]}
 fi
