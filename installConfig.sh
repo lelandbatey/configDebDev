8b1dfe019bf4a334c5ba1c9870861b7513df12cc
lelandbatey <lelandbatey@gmail.com>
2012-11-15 16:12:53 -0800

    Created short script for auto installing config files

---
+++ installConfig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Short script to install the various config files
+# Arguments are:
+#  .basrc
+#  .vim
+#  <nothing>
+
+
+if [ "$1" == .bashrc ]; then
+  cp .bashrc ~/
+elif [ "$1" == vim ]; then
+  cp -r .vim ~/ &
+  cp .vimrc ~/
+elif [ -z "$1" ]; then
+  cp .bashrc ~/
+  cp -r .vim ~/
+  cp .vimrc ~/
+  cp .Xresources ~/
+fi
